4f062b264503ee53caf71562bb038746fbea8804
Plum <pplluumm@users.noreply.github.com>
2020-01-15 21:42:49 -0800

    GoogleMap Progress and Icons

--- AndroidStudio/GoogleMap/app/src/main/java/com/betcher/jordan/googlemap/MainActivity.java
+++ AndroidStudio/GoogleMap/app/src/main/java/com/betcher/jordan/googlemap/MainActivity.java
@@ -1,14 +1,46 @@
 package com.betcher.jordan.googlemap;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class MainActivity extends AppCompatActivity {
+
+    private GoogleMap mMap;
+
+    //Avast might be stopping the app from working
+    //https://forum.avast.com/index.php?topic=197705.0
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //*
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        //*
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap = googleMap;
+
+
+                // Add a marker in Sydney, Australia, and move the camera.
+                LatLng sydney = new LatLng(-34, 151);
+                mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            }
+        });
+        //*/
     }
 }
