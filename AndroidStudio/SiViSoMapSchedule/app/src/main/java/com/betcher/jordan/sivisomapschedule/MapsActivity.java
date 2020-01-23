ca9534684179e3747eba1f9436e5b1450c7b7853
Plum <pplluumm@users.noreply.github.com>
2020-01-23 11:52:18 -0800

    Prints the first 10 locations that are closest to the map's latitude and longitude

--- AndroidStudio/SiViSoMapSchedule/app/src/main/java/com/betcher/jordan/sivisomapschedule/MapsActivity.java
+++ AndroidStudio/SiViSoMapSchedule/app/src/main/java/com/betcher/jordan/sivisomapschedule/MapsActivity.java
@@ -2,6 +2,8 @@ package com.betcher.jordan.sivisomapschedule;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,7 +17,9 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 {
@@ -57,6 +61,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 		LatLng sydney = new LatLng(-34, 151);
 		mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
 		mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+		Geocoder geocoder = new Geocoder(this);
+		LatLng cameraLocation = mMap.getCameraPosition().target;
+		try
+		{
+			List<Address> addresses = geocoder.getFromLocation(cameraLocation.latitude, cameraLocation.longitude, 10);
+			
+			for(Address address : addresses)
+			{
+				Log.d("TEST", "Address: " + address);
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 	
 	public void onClick(View view)
