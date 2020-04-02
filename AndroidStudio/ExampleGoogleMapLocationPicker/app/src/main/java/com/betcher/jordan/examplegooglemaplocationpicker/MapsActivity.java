ac440a63f051155bec9e02f15ae378ccac09167c
Plum <pplluumm@users.noreply.github.com>
2020-04-02 11:48:08 -0700

    ExampleGoogleMapLocationPicker layout
    Created layout

--- AndroidStudio/ExampleGoogleMapLocationPicker/app/src/main/java/com/betcher/jordan/examplegooglemaplocationpicker/MapsActivity.java
+++ AndroidStudio/ExampleGoogleMapLocationPicker/app/src/main/java/com/betcher/jordan/examplegooglemaplocationpicker/MapsActivity.java
@@ -20,7 +20,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_maps);
+		setContentView(R.layout.layout_location_picker);
 		// Obtain the SupportMapFragment and get notified when the map is ready to be used.
 		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
 				.findFragmentById(R.id.map);
