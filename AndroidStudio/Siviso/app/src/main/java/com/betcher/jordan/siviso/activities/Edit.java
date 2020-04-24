20fb1403cb950098d6c91baa18a09f172e810a9e
Plum <pplluumm@users.noreply.github.com>
2020-04-24 11:26:40 -0700

    Siviso Edit page

---
+++ AndroidStudio/Siviso/app/src/main/java/com/betcher/jordan/siviso/activities/Edit.java
@@ -0,0 +1,28 @@
+package com.betcher.jordan.siviso.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.betcher.jordan.siviso.R;
+
+public class Edit extends AppCompatActivity
+{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_edit);
+	}
+	
+	public void onClickButtonCancel(View view)
+	{
+	}
+	
+	public void onClickButtonConfirmEdit(View view)
+	{
+		
+	}
+}
