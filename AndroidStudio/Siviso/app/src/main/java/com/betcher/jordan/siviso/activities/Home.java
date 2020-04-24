a02c350e273e2841fc04a5f55200a4dd1158126f
Plum <pplluumm@users.noreply.github.com>
2020-04-24 10:43:54 -0700

    Siviso Ported over the home page

---
+++ AndroidStudio/Siviso/app/src/main/java/com/betcher/jordan/siviso/activities/Home.java
@@ -0,0 +1,34 @@
+package com.betcher.jordan.siviso.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.betcher.jordan.siviso.R;
+
+public class Home extends AppCompatActivity
+{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+	}
+	
+	public void onClickButtonAdd(View view)
+	{
+	
+	}
+	
+	public void onClickButtonDelete(View view)
+	{
+	
+	}
+	
+	public void onClickButtonEdit(View view)
+	{
+	
+	}
+}
