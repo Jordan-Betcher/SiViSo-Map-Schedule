b74aae1154584c997458f2a83cead833b0a013f5
Plum <pplluumm@users.noreply.github.com>
2020-04-24 11:21:02 -0700

    Siviso add page

---
+++ AndroidStudio/Siviso/app/src/main/java/com/betcher/jordan/siviso/activities/Add.java
@@ -0,0 +1,27 @@
+package com.betcher.jordan.siviso.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.betcher.jordan.siviso.R;
+
+public class Add extends AppCompatActivity
+{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add);
+	}
+	
+	public void onClickButtonCancel(View view)
+	{
+	}
+	
+	public void onClickButtonConfirmAdd(View view)
+	{
+	}
+}
