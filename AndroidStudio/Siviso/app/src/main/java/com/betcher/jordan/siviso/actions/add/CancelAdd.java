2e3905d56a7b8c995f69552654d389fd523a1d7b
Plum <pplluumm@users.noreply.github.com>
2020-04-24 12:17:19 -0700

    Siviso CancelAdd
    Cancels out of the Add Activity when Cancel is pressed

---
+++ AndroidStudio/Siviso/app/src/main/java/com/betcher/jordan/siviso/actions/add/CancelAdd.java
@@ -0,0 +1,11 @@
+package com.betcher.jordan.siviso.actions.add;
+
+import com.betcher.jordan.siviso.activities.Add;
+
+public class CancelAdd
+{
+	public static void run(Add activityAdd)
+	{
+		activityAdd.finish();
+	}
+}
--- AndroidStudio/Siviso/app/src/main/java/com/betcher/jordan/siviso/activities/Add.java
+++ AndroidStudio/Siviso/app/src/main/java/com/betcher/jordan/siviso/activities/Add.java
@@ -6,12 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.betcher.jordan.siviso.R;
-import com.betcher.jordan.siviso.actions.SetMapAddPosition;
-import com.google.android.gms.maps.CameraUpdateFactory;
+import com.betcher.jordan.siviso.actions.add.CancelAdd;
+import com.betcher.jordan.siviso.actions.add.SetMapAddPosition;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
 
 public class Add extends AppCompatActivity
 {
@@ -40,7 +39,7 @@ public class Add extends AppCompatActivity
 	
 	public void onClickButtonCancel(View view)
 	{
-	
+		CancelAdd.run(this);
 	}
 	
 	public void onClickButtonConfirmAdd(View view)
